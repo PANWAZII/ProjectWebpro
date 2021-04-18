00a525d7fd7f3fcab5e643fa1ab6a27f22fecda6
Panwa Muangsong <panwa1379@gmail.com>
2021-04-19 05:45:53 +0700

    add connection for comment

--- connectDB.php
+++ connectDB.php
@@ -9,7 +9,9 @@
     $conn = mysqli_connect($servername, $username, $password, $dbname);
     // Create Connection for userID
     $con = new mysqli('localhost', 'root', '', 'projectwebpro');
-
+    // Create Connection for comment
+    $conforcmt = new PDO("mysql:host={$servername};dbname={$dbname};", $username, $password);
+    $conforcmt->exec("set names utf8");
     // Check connection
     if (!$conn) {
         die("Connection failed" . mysqli_connect_error());
