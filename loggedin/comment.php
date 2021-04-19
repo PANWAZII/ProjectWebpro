4a1824444c93e395aee82092fda5ed35be64847e
Panwa Muangsong <panwa1379@gmail.com>
2021-04-19 07:23:35 +0700

    fixed comment system

--- loggedin/comment.php
+++ loggedin/comment.php
@@ -9,61 +9,18 @@ if (!isset($_SESSION['username'])) {
 if (isset($_GET['logout'])) {
     session_destroy();
     unset($_SESSION['username']);
+    unset($_SESSION['userID']);
     header('location: /ProjectWebpro/login.php');
 }
 
-function createCommentRow($data)
-{
-    global $conn;
-
-    $response = '
-            <div class="comment">
-                <div class="user">' . $data['username'] . ' <span class="time">' . $data['createdOn'] . '</span></div>
-                <div class="userComment">' . $data['comment'] . '</div>
-                <div class="reply"><a href="javascript:void(0)" data-commentID="' . $data['id'] . '" onclick="reply(this)">REPLY</a></div>
-                <div class="replies">';
-
-    $sql = $conn->query("SELECT replies.id, username, comment, DATE_FORMAT(replies.createdOn, '%Y-%m-%d') AS createdOn FROM replies INNER JOIN user ON replies.userID = user.id WHERE replies.commentID = '" . $data['id'] . "' ORDER BY replies.id DESC LIMIT 1");
-    while ($dataR = $sql->fetch_assoc())
-        $response .= createCommentRow($dataR);
-
-    $response .= '
-                        </div>
-            </div>
-        ';
-
-    return $response;
+if (isset($_POST['comment'])) {
+    $username = $_SESSION['username'];
+    $comment = mysqli_real_escape_string($conn, $_POST['comment']);
+    $sql = "INSERT INTO comments (username, comment, createdOn) 
+    VALUES ('$username','$comment', NOW())";
+    mysqli_query($conn, $sql);
 }
 
-if (isset($_POST['getAllComments'])) {
-    $start = $conn->real_escape_string($_POST['start']);
-
-    $response = "";
-    $sql = $conn->query("SELECT comments.id, username, comment, DATE_FORMAT(comments.createdOn, '%Y-%m-%d') AS createdOn FROM comments INNER JOIN user ON comments.userID = user.id ORDER BY comments.id DESC LIMIT $start, 20");
-    while ($data = $sql->fetch_assoc())
-        $response .= createCommentRow($data);
-
-    exit($response);
-}
-
-if (isset($_POST['addComment'])) {
-    $comment = $conn->real_escape_string($_POST['comment']);
-    $isReply = $conn->real_escape_string($_POST['isReply']);
-    $commentID = $conn->real_escape_string($_POST['commentID']);
-
-    if ($isReply != 'false') {
-        $conn->query("INSERT INTO replies (comment, commentID, userID, createdOn) VALUES ('$comment', '$commentID', '" . $_SESSION['userID'] . "', NOW())");
-        $sql = $conn->query("SELECT replies.id, username, comment, DATE_FORMAT(replies.createdOn, '%Y-%m-%d') AS createdOn FROM replies INNER JOIN user ON replies.userID = user.id ORDER BY replies.id DESC LIMIT 1");
-    } else {
-        $conn->query("INSERT INTO comments (userID, comment, createdOn) VALUES ('" . $_SESSION['userID'] . "','$comment',NOW())");
-        $sql = $conn->query("SELECT comments.id, username, comment, DATE_FORMAT(comments.createdOn, '%Y-%m-%d') AS createdOn FROM comments INNER JOIN user ON comments.userID = user.id ORDER BY comments.id DESC LIMIT 1");
-    }
-
-    $data = $sql->fetch_assoc();
-    exit(createCommentRow($data));
-}
-$sqlNumComments = $conn->query("SELECT id FROM comments");
-$numComments = $sqlNumComments->num_rows;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -74,28 +31,117 @@ $numComments = $sqlNumComments->num_rows;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
+        @import url('https://fonts.googleapis.com/css2?family=Varela+Round&display=swap');
+
+        @keyframes fadein {
+            from {
+                opacity: 0;
+            }
+
+            to {
+                opacity: 1;
+            }
+        }
+
+        @keyframes blink {
+            0% {
+                color: rgba(0, 136, 169, 1);
+            }
+
+            50% {
+                color: orange;
+            }
+
+            100% {
+                color: rgba(0, 136, 169, 1);
+            }
+
+        }
+
         body {
             background-color: rgb(50, 50, 50);
+            font-family: 'Varela Round', sans-serif;
+            font-weight: 400;
+            font-size: 20px;
+            color: white;
         }
 
-        .comment {
-            margin-top: 300px;
-            width: 800px;
-            height: 400px;
-        }
-
-        .textarea {
-            width: 300px;
-            height: 40px;
+        .usercomment {
+            background-color: whitesmoke;
+            margin-top: 10px;
+            font-family: 'Varela Round', sans-serif;
+            font-weight: 400;
+            font-size: 20px;
+            width: 600px;
+            height: 100px;
             border: none;
             border-radius: 10px;
         }
 
+        .textarea {
+            overflow: auto;
+            width: 400px;
+            height: 40px;
+            border: none;
+            border-radius: 10px;
+            cursor: pointer;
+        }
+
         .container {
+            float: left;
+            animation: fadein 1s;
+            background-color: rgb(25, 25, 25);
+            margin-top: 100px;
+            width: 900px;
+            height: 800px;
+            border-radius: 30px;
+        }
+
+        .submitbtn {
+            border: none;
+            border-radius: 10px;
+            width: 600px;
+            height: 30px;
+            background-color: rgba(0, 136, 169, 1);
+            font-family: 'Varela Round', sans-serif;
+            font-weight: 400;
+            font-size: 20px;
+        }
+
+        .submitbtn:hover {
+            background-color: orange;
+        }
+
+        ::placeholder {
+            color: orange;
+        }
+
+        iframe {
+            margin-top: 40px;
+            width: 800px;
+            height: 600px;
+            border: none;
+            border-radius: 10px;
+        }
+
+        .header {
+            writing-mode: vertical-rl;
+            text-orientation: sideways;
+            float: left;
+            margin-left: 60px;
+            margin-top: 220px;
+            width: 40px;
+            height: 400px;
+            font-size: 100px;
+            color: rgba(0, 136, 169, 1);
+            animation: fadein 2s, blink 2s infinite;
+        }
+
+        .makecontentcenter {
+            display: flex;
             justify-content: center;
         }
     </style>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 </head>
 
@@ -108,102 +154,26 @@ $numComments = $sqlNumComments->num_rows;
         ?>
     </div>
     <br>
-    <div class="container">
-        <center>
-            <div class="comment">
-                <div class="row">
-                    <div class="">
-                        <textarea class="textarea" id="mainComment" placeholder="Add Public Comment" cols="30" rows="2"></textarea><br>
-                        <button style="float:right" onclick="isReply = false;" id="addComment">Add Comment</button>
+    <center>
+        <div class="makecontentcenter">
+            <form action="comment.php" method="post">
+                <center>
+                    <div class="container">
+                        <iframe src="/ProjectWebpro/loggedin/fetchcmt.php" frameborder="0"></iframe>
+                        <textarea class="usercomment" placeholder="What do you think about my website?" name="comment" id="comment" cols="5" rows="10"></textarea>
+                        <br>
+                        <input class="submitbtn" type="submit" value="POST"></input>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="">
-                        <h2><b id="numComments"><?php echo $numComments ?> Comments</b></h2>
-                        <div class="userComments"></div>
-                    </div>
-                </div>
-            </div>
-            <div class="" style="display:none">
-                <div class="">
-                    <textarea class="" id="replyComment" placeholder="Add Public Comment" cols="30" rows="2"></textarea><br>
-                    <button style="float:right" onclick="isReply = true;" id="addReply">Add Reply</button>
-                    <button style="float:right" onclick="$('.replyRow').hide();">Close</button>
-                </div>
-            </div>
-        </center>
-    </div>
-
-    <script type="text/javascript">
-        var isReply = false,
-            commentID = 0,
-            max = <?php echo $numComments ?>;
-        $(document).ready(function() {
-            $("#addComment, #addReply").on('click', function() {
-                var comment;
-
-                if (!isReply)
-                    comment = $("#mainComment").val();
-                else
-                    comment = $("#replyComment").val();
-
-                if (comment.length > 5) {
-                    $.ajax({
-                        url: '/ProjectWebpro/loggedin/comment.php',
-                        method: 'POST',
-                        dataType: 'text',
-                        data: {
-                            addComment: 1,
-                            comment: comment,
-                            isReply: isReply,
-                            commentID: commentID
-                        },
-                        success: function(response) {
-                            max++;
-                            $("#numComments").text(max + " Comments");
-
-                            if (!isReply) {
-                                $(".userComments").prepend(response);
-                                $("#mainComment").val("");
-                            } else {
-                                commentID = 0;
-                                $("#replyComment").val("");
-                                $(".replyRow").hide();
-                                $('.replyRow').parent().next().append(response);
-                            }
-                        }
-                    });
-                } else
-                    alert('Please Check Your Inputs');
-            });
-        });
-
-        function reply(caller) {
-            commentID = $(caller).attr('data-commentID');
-            $(".replyRow").insertAfter($(caller));
-            $('.replyRow').show();
-        }
-
-        function getAllComments(start, max) {
-            if (start > max) {
-                return;
-            }
-
-            $.ajax({
-                url: '/ProjectWebpro/loggedin/comment.php',
-                method: 'POST',
-                dataType: 'text',
-                data: {
-                    getAllComments: 1,
-                    start: start
-                },
-                success: function(response) {
-                    $(".userComments").append(response);
-                    getAllComments((start + 20), max);
-                }
-            });
-        }
-    </script>
+                </center>
+            </form>
+            <div class="header">COMMENT</div>
+        </div>
+    </center>
+    <?php
+    $path = $_SERVER['DOCUMENT_ROOT'];
+    $path .= "/ProjectWebpro/footer/footer.html";
+    include_once($path);
+    ?>
 </body>
 
 </html>
---
+++ loggedin/fetchcmt.php
@@ -0,0 +1,127 @@
+<?php
+session_start();
+include('../connectDB.php');
+if (!isset($_SESSION['username'])) {
+    $_SESSION['msg'] = "You must log in first";
+    header('location: /ProjectWebpro/login.php');
+}
+
+if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['username']);
+    unset($_SESSION['userID']);
+    header('location: /ProjectWebpro/login.php');
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Varela+Round&display=swap');
+
+        body {
+            background-color: rgb(50, 50, 50);
+            font-family: 'Varela Round', sans-serif;
+            font-weight: 400;
+            font-size: 20px;
+            color: white;
+        }
+
+        .usercomment {
+            background-color: gray;
+            margin-top: 450px;
+            font-family: 'Varela Round', sans-serif;
+            font-weight: 400;
+            font-size: 20px;
+            width: 600px;
+            height: 100px;
+            border: none;
+            border-radius: 10px;
+        }
+
+        .card {
+            margin-left: 20px;
+            padding: 5px;
+            word-wrap: break-word;
+            justify-content: left;
+            margin-bottom: 10px;
+            max-width: 650px;
+            min-width: 100px;
+            min-height: 40px;
+            display: inline-block;
+            border: none;
+            border-radius: 10px;
+            background-color: rgba(0, 136, 169, 1);
+        }
+
+        .date {
+            color: white;
+            font-size: 15px;
+            margin-top: 4px;
+            margin-left: 10px;
+        }
+
+        .buttomline {
+            margin-top: 10px;
+            width: 670px;
+            height: 2px;
+            background-color: black;
+        }
+
+        .namecolor {
+            display: flex;
+            color: orange;
+        }
+
+
+        ::-webkit-scrollbar {
+            width: 20px;
+        }
+
+
+        ::-webkit-scrollbar-track {
+            box-shadow: inset 0 0 5px grey;
+            /* border-radius: 10px; */
+        }
+
+        ::-webkit-scrollbar-thumb {
+            background: rgba(0, 136, 169, 1);
+        }
+
+        ::-webkit-scrollbar-thumb:hover {
+            background: orange;
+        }
+    </style>
+
+</head>
+
+<body>
+
+    <?php
+    $SQL = 'SELECT * FROM comments';
+    $stmt = $conforcmt->prepare($SQL);
+    $stmt->execute();
+    if ($stmt->rowCount() > 0) {
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    ?>
+            <p>
+            <div class="namecolor">@<?= $row['username']; ?></p>
+                <div class="date"><?php echo date('m/d/Y H:i:s', strtotime($row['createdOn'])); ?></div>
+            </div>
+            <div class="card">
+                <p><?= $row['comment']; ?></p>
+            </div>
+            <center>
+                <div class="buttomline"></div>
+            </center>
+    <?php }
+    } ?>
+</body>
+
+</html>
